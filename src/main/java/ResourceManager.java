53145fe1e91b92071b467e9b5eab1599a95182a7
GenDes <teryaevda@mail.ru>
2022-08-15 22:28:59 +0300

    Add Telegram ResManagers

--- src/main/java/ResourceManager.java
+++ src/main/java/ResourceManager.java
@@ -6,14 +6,14 @@ public class ResourceManager {
 
     static {
         MAP_RESOURCE_MANAGER.put(0, "-");
-        MAP_RESOURCE_MANAGER.put(1, "Марченко Ирина \n ТГ: ");
+        MAP_RESOURCE_MANAGER.put(1, "Марченко Ирина \nТГ: @abuzinaa");
         MAP_RESOURCE_MANAGER.put(2, "-");
-        MAP_RESOURCE_MANAGER.put(3, "Кузнецова Алена \n ТГ: ");
-        MAP_RESOURCE_MANAGER.put(4, "Пауткина Татьяна \n ТГ: ");
+        MAP_RESOURCE_MANAGER.put(3, "Кузнецова Алена \nТГ: @alenka_smith_TF");
+        MAP_RESOURCE_MANAGER.put(4, "Пауткина Татьяна \nТГ: @TPautkina_TeamForce");
         MAP_RESOURCE_MANAGER.put(5, "-");
         MAP_RESOURCE_MANAGER.put(6, "-");
         MAP_RESOURCE_MANAGER.put(7, "-");
-        MAP_RESOURCE_MANAGER.put(8, "Майоров Лев \n ТГ: ");
+        MAP_RESOURCE_MANAGER.put(8, "Майоров Лев \nТГ: @leothe1");
     }
     public static String getResourceManager(Integer numResourceManager){
         return MAP_RESOURCE_MANAGER.get(numResourceManager);
